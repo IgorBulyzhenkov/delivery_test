ee241d4c0446d08239153a311cad384828865299
Igor Bulyzhenkov <bulyzhenkov.igor@gmail.com>
2024-04-21 22:02:01 +0300

    add folder api request

--- app/Http/Controllers/Api/ParcelsController.php
+++ app/Http/Controllers/Api/ParcelsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Http\Requests\ParcelsRequest;
+use App\Http\Requests\Api\ParcelsRequest;
 use App\Models\Api\Parcels;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Validator;
